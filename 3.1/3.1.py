7a0e8d0a9a108f82fa634c4be1959c8926e9f026
VikaBrown43 <156464311+VikaBrown43@users.noreply.github.com>
2024-01-12 22:33:15 +0300

    Add files via upload

---
+++ 3.1/3.1.py
@@ -0,0 +1,4 @@
+pet_type = input ("Введите вид питомца: ")
+pet_name = input ("Введите кличку питомца: ")
+pet_age = input ("Введите возраст: ")
+print("У вас ", pet_type, " Кличка ", pet_name , " Возраст: ", pet_age)
